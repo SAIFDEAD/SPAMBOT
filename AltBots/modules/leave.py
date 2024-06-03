298e190497a60a49a351fa2b9ef243d5754f4e69
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2024-06-03 22:00:03 +0530

    ⚡ 𝐒αιғβσтƨ ⚡

--- AltBots/modules/leave.py
+++ AltBots/modules/leave.py
@@ -1,4 +1,4 @@
-from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, OWNER_ID, CMD_HNDLR as hl
+from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, SUDO_USERS, CMD_HNDLR as hl
 
 from telethon import events
 from telethon.tl.functions.channels import LeaveChannelRequest
@@ -15,9 +15,10 @@ from telethon.tl.functions.channels import LeaveChannelRequest
 @X9.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
 @X10.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
 async def leave(e):
-    if e.sender_id in OWNER_ID:
+    if e.sender_id in SUDO_USERS:
+
         if len(e.text) > 7:
-            event = await e.reply("» ʟᴇᴀᴠɪɴɢ ʙᴏss. ..")
+            event = await e.reply("» ʟᴇᴀᴠɪɴɢ...")
             mkl = e.text.split(" ", 1)
             try:
                 await event.client(LeaveChannelRequest(int(mkl[1])))
@@ -28,7 +29,7 @@ async def leave(e):
                   alt = f"**» ʏᴏᴜ ᴄᴀɴ'ᴛ ᴅᴏ ᴛʜɪꜱ ʜᴇʀᴇ !!**\n\n» {hl}leave <ᴄʜᴀɴɴᴇʟ/ᴄʜᴀᴛ ɪᴅ> \n» {hl}leave : ᴛʏᴘᴇ ɪɴ ᴛʜᴇ ɢʀᴏᴜᴘ, ʙᴏᴛ ᴡɪʟʟ ᴀᴜᴛᴏ ʟᴇᴀᴠᴇ ᴛʜᴀᴛ ɢʀᴏᴜᴘ."
                   await e.reply(alt)
              else:
-                  event = await e.reply("» ʟᴇᴀᴠɪɴɢ ʙᴏss ...")
+                  event = await e.reply("» ʟᴇᴀᴠɪɴɢ...")
                   try:
                       await event.client(LeaveChannelRequest(int(e.chat_id)))
                   except Exception as e:
